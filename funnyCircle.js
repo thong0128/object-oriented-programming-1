1ec6d76a78c727264728c4c498de0a8b9aa937a3
thong0128 <thong0128@gmail.com>
2023-11-24 17:17:52 +0700

    add funnyCircle

---
+++ funnyCircle.js
@@ -0,0 +1,64 @@
+class Circle {
+    constructor(x, y, radius){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+}
+
+// function createCircle(){
+//     let ctx = document.getElementById("myCanvas").getContext("2d");
+//     let circle = new Circle(500, 500, 80);
+//     ctx.beginPath();
+//     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+//     ctx.fill();
+// }
+
+// function createCircle(){
+//     let ctx = document.getElementById("myCanvas").getContext("2d");
+//     let radius = Math.floor(Math.random() * 80);
+//     let circle = new Circle(500, 500, radius);
+//     ctx.beginPath();
+//     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+//     ctx.fill();
+// }
+function getRandomHex(){
+    return Math.floor(Math.random()*255);
+}
+
+function getRandomColor(){
+    let red = getRandomHex();
+    let green = getRandomHex();
+    let blue = getRandomHex();
+    return "rgb(" + red + "," + blue + "," + green +")";
+}
+// function createCircle(){
+//     let ctx = document.getElementById("myCanvas").getContext("2d");
+//     let radius = Math.floor(Math.random() * 80);
+//     let color = getRandomColor();
+//     let circle = new Circle(500, 500, radius);
+//     ctx.beginPath();
+//         ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+//         ctx.fillStyle = color;
+//         ctx.fill();
+// }
+function createCircle(){
+    let ctx = document.getElementById("myCanvas").getContext("2d");
+    let radius = Math.floor(Math.random() * 80);
+    let color = getRandomColor();
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+    let circle = new Circle(x, y, radius);
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+createCircle();
+function createMultipleCircle(){
+    for(let i = 0; i < 30; i++){
+        createCircle();
+    }
+}
+
+createMultipleCircle();
